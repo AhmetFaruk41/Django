48a5616399a1e1dd4ca89da1032c1768ffdf3e3c
Ahmet Faruk ÇATLAR <ahmetfaruk411@gmail.com>
2024-04-04 03:03:52 +0300

    partials

--- courses/views.py
+++ courses/views.py
@@ -6,36 +6,40 @@ data = {
     "web-geliştirme":"web geliştirme kategorisine ait kurslarr",
     "mobil":"mobil kategorisine ait kurslar",
 }
-
-def kurslar(request):
-    category_list = list(data.keys())
-    list_items=''
-    for category in category_list:
-        redirect_url = reverse('courses_by_category',args=[category])
-        list_items += f"<li><a href='{redirect_url}>{category}</a></li>"
-    htmt= f"<h1>kurs listesi</h1><br><ul>{list_items}</ul>"
-
-    return HttpResponse(htmt)
-
 def index(request):
     return render(request,'index.html')
+
+def kurslar(request):
+    list_items=""
+    category_list = list(data.keys())
+
+    for category in category_list:
+        redirect_url = reverse('courses_by_category', args=[category])
+        list_items += f"<li><a href='{redirect_url}'>{category}</a></li>"
+    html= f"<h1>kurs listesi</h1><br><ul>{list_items}</ul>"
+
+    return HttpResponse(html)
 
 def details(requst,kurs_adi):
     return HttpResponse(f"{kurs_adi} detay sayfası")
 
-def getCoursesByCategory(request,category_name):
+def getCoursesByCategory(request, category_name):
     try:
         category_text = data[category_name]
-        return HttpResponse(category_text)
+        return render(request,'courses/kurslar.html',{
+            'category':category_name,
+            'category_text':category_text
+        })
     except:
         return HttpResponseNotFound("<h1>yanlış kategori seçimi</h1>")
     
 def getCoursesByCategoryID(request,category_id):
     category_list = list(data.keys())
-    if category_id>len(category_list):
+    if (category_id>len(category_list)):
         return HttpResponseNotFound("yanlış kategori seçimi")
     
     category_name = category_list[category_id - 1]
+
     redirect_url = reverse('courses_by_category',args=[category_name])
     
     return redirect(redirect_url)
--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'pages/index.html')
 
 def about(request):
-    return render(request,'about.html')
+    return render(request,'pages/about.html')
 
 def contact(request):
-    return render(request,'contact.html')
+    return render(request,'pages/contact.html')
 
